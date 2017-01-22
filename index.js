186551841e01330f13a3e026b4bd28e7d981083d
Bernard Espanola <bespanola@Bernards-MacBook-Pro.local>
2017-01-21 21:46:42 -0800

    Added handlebars package.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const http = require('express')
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
-const port = 3000
+
+app.engine('.hbs', exhbs({
+  defaultLayout: 'main',
+  extname:  '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, resp) => {
-  resp.send('Hello from Express!')
+  response.render('home', {
+    name: 'Bernard'
+  })
+})
+
+app.use((err, req, resp, next) => {
+  console.log(err)
+  resp.status(500).send('Something Broke')
 })
 
 app.listen(port, (err) => {
